bac4526a4ba0508aeaa93d67a4ffd637ef168421
Eric St-Pierre <estpier@gmail.com>
2014-10-26 10:46:19 -0400

    listing vehicule affiche le data

--- src/controller/VehiculeListController.java
+++ src/controller/VehiculeListController.java
@@ -1,22 +1,92 @@
 package controller;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import model.Vehicule;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.DBClass;
+import model.Vehicule;
 
-public class VehiculeListController implements Initializable{
+public class VehiculeListController implements Initializable {
 
+	@FXML
+	private TableView<Vehicule> tableVehicule;
+	@FXML
+	private TableColumn<Vehicule, String> noStockCol;
+	@FXML
+	private TableColumn<Vehicule, String> noSerieCol;
+	@FXML
+	private TableColumn<Vehicule, Integer> anneeCol;
+	@FXML
+	private TableColumn<Vehicule, String> marqueCol;
+	@FXML
+	private TableColumn<Vehicule, String> modeleCol;
+	@FXML
+	private TableColumn<Vehicule, String> couleurCol;
+	@FXML
+	private TableColumn<Vehicule, String> transmissionCol;
+	@FXML
+	private TableColumn<Vehicule, Integer> kilometrageCol;
+	
+	private ObservableList<Vehicule> listAllVehicule;
+	@FXML
+	private ToggleButton estVenduToggle;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+
+		try {
+			listAllVehicule = Vehicule.listAllVehicule();
+			fillVehiculeTable(listAllVehicule);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
-	}
-	
-	public void testDb(ActionEvent event){
-		Vehicule.listAllVehicule();
+
 	}
 
+	public void EstVenduClick(){
+		if(estVenduToggle.getText()=="Oui"){
+			estVenduToggle.setText("Non");
+		}else{
+			estVenduToggle.setText("Oui");
+		}
+	}
+	
+	@FXML
+	public void fillVehiculeTable(ObservableList<Vehicule> listeVehicule){
+		/*assert tableVehicule != null : "fx:id=\"tableVehicule\" was not injected: check your FXML file 'Véhiculelist.fxml'.";
+		*/
+		try {
+			noStockCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("stockNumber"));
+			noSerieCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("noSerie"));
+			anneeCol.setCellValueFactory(new PropertyValueFactory<Vehicule,Integer>("annee"));
+			marqueCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("marque"));
+			modeleCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("modele"));
+			couleurCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("couleur"));
+			transmissionCol.setCellValueFactory(new PropertyValueFactory<Vehicule,String>("transmission"));
+			kilometrageCol.setCellValueFactory(new PropertyValueFactory<Vehicule,Integer>("kilometrage"));
+			tableVehicule.setItems(listeVehicule);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getStackTrace());
+		}
+	
+	
+	}
+	
+	
 }
--- src/model/Vehicule.java
+++ src/model/Vehicule.java
@@ -7,6 +7,9 @@ import java.util.Currency;
 import java.util.Date;
 import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import com.mysql.jdbc.PreparedStatement;
 import com.mysql.jdbc.Statement;
 
@@ -14,36 +17,40 @@ public class Vehicule {
 	private String noSerie;
 	private String marque;
 	private String modele;
-	private String annee;
+	private int annee;
 	private String couleur;
 	private String typeVendeur;
-	private Date dateAchat;
+	private String dateAchat;
 	private String noVendeur;
 	private boolean estVendu;
-	private Currency prixVente;
+	private BigDecimal prixVente;
 	private String typeAcheteur;
 	private String noAcheteur;
-	private Currency prixAcheteur;
-	private Date dateVente;
+	private BigDecimal prixAcheteur;
+	private String dateVente;
 	private boolean aEuEchange;
 	private boolean aEuEntretien;
 	private String transmission;
 	private String noClef;
-	private long kilometrage;
+	private int kilometrage;
 	private String cilyndre;
 	private String stockNumber;
 	private String Cheque;
-	private Currency prixClient;
+	private BigDecimal prixClient;
 	private boolean venteAvecContrat;
 
-	public Vehicule(String noSerie, String marque, String modele, String annee,
-			String couleur, String typeVendeur, Date dateAchat,
-			String noVendeur, boolean estVendu, Currency prixVente,
-			String typeAcheteur, String noAcheteur, Currency prixAcheteur,
-			Date dateVente, boolean aEuEchange, boolean aEuEntretien,
-			String transmission, String noClef, long kilometrage,
+	public Vehicule() {
+
+	}
+
+	public Vehicule(String noSerie, String marque, String modele, int annee,
+			String couleur, String typeVendeur, String dateAchat,
+			String noVendeur, boolean estVendu, BigDecimal prixVente,
+			String typeAcheteur, String noAcheteur, BigDecimal prixAcheteur,
+			String dateVente, boolean aEuEchange, boolean aEuEntretien,
+			String transmission, String noClef, int kilometrage,
 			String cilyndre, String stockNumber, String cheque,
-			Currency prixClient, boolean venteAvecContrat) {
+			BigDecimal prixClient, boolean venteAvecContrat) {
 		this.noSerie = noSerie;
 		this.marque = marque;
 		this.modele = modele;
@@ -69,9 +76,204 @@ public class Vehicule {
 		this.prixClient = prixClient;
 		this.venteAvecContrat = venteAvecContrat;
 	}
+	
+	
 
-	public static List<Vehicule> listAllVehicule() {
-		List<Vehicule> vehiculeList = new ArrayList<Vehicule>();
+	public String getNoSerie() {
+		return noSerie;
+	}
+
+	public void setNoSerie(String noSerie) {
+		this.noSerie = noSerie;
+	}
+
+	public String getMarque() {
+		return marque;
+	}
+
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+
+	public String getModele() {
+		return modele;
+	}
+
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+
+	public String getTypeVendeur() {
+		return typeVendeur;
+	}
+
+	public void setTypeVendeur(String typeVendeur) {
+		this.typeVendeur = typeVendeur;
+	}
+
+	public String getDateAchat() {
+		return dateAchat;
+	}
+
+	public void setDateAchat(String dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+	public String getNoVendeur() {
+		return noVendeur;
+	}
+
+	public void setNoVendeur(String noVendeur) {
+		this.noVendeur = noVendeur;
+	}
+
+	public boolean isEstVendu() {
+		return estVendu;
+	}
+
+	public void setEstVendu(boolean estVendu) {
+		this.estVendu = estVendu;
+	}
+
+	public BigDecimal getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(BigDecimal prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public String getTypeAcheteur() {
+		return typeAcheteur;
+	}
+
+	public void setTypeAcheteur(String typeAcheteur) {
+		this.typeAcheteur = typeAcheteur;
+	}
+
+	public String getNoAcheteur() {
+		return noAcheteur;
+	}
+
+	public void setNoAcheteur(String noAcheteur) {
+		this.noAcheteur = noAcheteur;
+	}
+
+	public BigDecimal getPrixAcheteur() {
+		return prixAcheteur;
+	}
+
+	public void setPrixAcheteur(BigDecimal prixAcheteur) {
+		this.prixAcheteur = prixAcheteur;
+	}
+
+	public String getDateVente() {
+		return dateVente;
+	}
+
+	public void setDateVente(String dateVente) {
+		this.dateVente = dateVente;
+	}
+
+	public boolean isaEuEchange() {
+		return aEuEchange;
+	}
+
+	public void setaEuEchange(boolean aEuEchange) {
+		this.aEuEchange = aEuEchange;
+	}
+
+	public boolean isaEuEntretien() {
+		return aEuEntretien;
+	}
+
+	public void setaEuEntretien(boolean aEuEntretien) {
+		this.aEuEntretien = aEuEntretien;
+	}
+
+	public String getTransmission() {
+		return transmission;
+	}
+
+	public void setTransmission(String transmission) {
+		this.transmission = transmission;
+	}
+
+	public String getNoClef() {
+		return noClef;
+	}
+
+	public void setNoClef(String noClef) {
+		this.noClef = noClef;
+	}
+
+	public int getKilometrage() {
+		return kilometrage;
+	}
+
+	public void setKilometrage(int kilometrage) {
+		this.kilometrage = kilometrage;
+	}
+
+	public String getCilyndre() {
+		return cilyndre;
+	}
+
+	public void setCilyndre(String cilyndre) {
+		this.cilyndre = cilyndre;
+	}
+
+	public String getStockNumber() {
+		return stockNumber;
+	}
+
+	public void setStockNumber(String stockNumber) {
+		this.stockNumber = stockNumber;
+	}
+
+	public String getCheque() {
+		return Cheque;
+	}
+
+	public void setCheque(String cheque) {
+		Cheque = cheque;
+	}
+
+	public BigDecimal getPrixClient() {
+		return prixClient;
+	}
+
+	public void setPrixClient(BigDecimal prixClient) {
+		this.prixClient = prixClient;
+	}
+
+	public boolean isVenteAvecContrat() {
+		return venteAvecContrat;
+	}
+
+	public void setVenteAvecContrat(boolean venteAvecContrat) {
+		this.venteAvecContrat = venteAvecContrat;
+	}
+
+	public static ObservableList<Vehicule> listAllVehicule() throws SQLException {
+		ObservableList<Vehicule> vehicules = FXCollections.observableArrayList();
+
 		PreparedStatement statement = null;
 		ResultSet resultSet = null;
 		// JDBC driver name and database URL
@@ -103,39 +305,66 @@ public class Vehicule {
 
 			// STEP 5: Extract data from result set
 			while (rs.next()) {
+				Vehicule vehicule = new Vehicule();
+				
 				// Retrieve by column name
 				String noSerie = rs.getString("fldNoSerie");
+				vehicule.setNoSerie(noSerie);
 				String marque = rs.getString("fldMarque");
+				vehicule.setMarque(marque);
 				String modele = rs.getString("fldModele");
+				vehicule.setModele(modele);
 				int annee = rs.getInt("fldAnnee");
+				vehicule.setAnnee(annee);
 				String couleur = rs.getString("fldNoPeinture");
+				vehicule.setCouleur(couleur);
 				String typeVendeur = rs.getString("fldTypeVendeur");
+				vehicule.setTypeVendeur(typeVendeur);
 				String dateAchat = rs.getString("fldDateAchat");
+				vehicule.setDateAchat(dateAchat);
 				String noVendeur = rs.getString("fldNovendeur");
+				vehicule.setNoVendeur(noVendeur);
 				boolean estVendu = rs.getBoolean("fldVente");
+				vehicule.setEstVendu(estVendu);
 				BigDecimal prixVente = rs.getBigDecimal("fldPrixVente");
+				vehicule.setPrixVente(prixVente);
 				String typeAcheteur = rs.getString("fldTypeAcheteur");
+				vehicule.setTypeAcheteur(typeAcheteur);
 				String noAcheteur = rs.getString("fldNoAcheteur");
+				vehicule.setNoAcheteur(noAcheteur);
 				BigDecimal prixAchat = rs.getBigDecimal("fldPrixAchat");
+				vehicule.setPrixAcheteur(prixAchat);
 				String dateVente = rs.getString("fldDateVente");
+				vehicule.setDateVente(dateVente);
 				boolean aEuEchange = rs.getBoolean("fldEchange");
+				vehicule.setaEuEchange(aEuEchange);
 				boolean aEuEntretien = rs.getBoolean("fldEntretien");
+				vehicule.setaEuEntretien(aEuEntretien);
 				String transmission = rs.getString("fldTransmission");
+				vehicule.setTransmission(transmission);
 				String noClef = rs.getString("fldNoClef");
+				vehicule.setNoClef(noClef);
 				int kilometrage = rs.getInt("fldKilometrage");
+				vehicule.setKilometrage(kilometrage);
 				String cylindre = rs.getString("fldCylindre");
+				vehicule.setCilyndre(cylindre);
 				String stockNumber = rs.getString("fldStockNumber");
+				vehicule.setStockNumber(stockNumber);
 				String cheque = rs.getString("fldCheque");
+				vehicule.setCheque(cheque);
 				BigDecimal prixClient = rs.getBigDecimal("fldPrixClient");
+				vehicule.setPrixClient(prixClient);
 				boolean venteAvecContrat = rs.getBoolean("fldVenteAvecContrat");
+				vehicule.setVenteAvecContrat(venteAvecContrat);
 
-				System.out.println(marque+" : "+dateAchat);
+				vehicules.add(vehicule);
+				
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			
+
 			if (rs != null) {
 				try {
 					rs.close();
@@ -143,21 +372,28 @@ public class Vehicule {
 					e.printStackTrace();
 				}
 			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 			
-			if(stmt!=null){try {
-				stmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}}
-
-			if(conn!=null){try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}}
+			
 		}
+		return vehicules;
 
-		return vehiculeList;
+		
 
 	}
 
