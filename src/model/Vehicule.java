5c152724a532cc9c86ffee8cb5ea7b91d2c6f23c
gpaweb <estpier@gmail.com>
2014-10-28 14:54:20 -0400

    Update Vehicule.java

--- src/model/Vehicule.java
+++ src/model/Vehicule.java
@@ -280,8 +280,8 @@ public class Vehicule {
 		final String url = "jdbc:mysql://localhost/gpa2k15?zeroDateTimeBehavior=convertToNull";
 		// Database credentials
 		final String dbName = "gpa2k15";
-		final String userName = "root";
-		final String dbPassword = "passwd12";
+		final String userName = "";
+		final String dbPassword = "";
 
 		Connection conn = null;
 		java.sql.Statement stmt = null;
