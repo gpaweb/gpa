e137f87f129955d108939f7c41cb0749691ca03a
Eric St-Pierre <estpier@gmail.com>
2014-11-25 19:39:53 -0500

    25 novembre pm

--- src/controller/VehiculeDetailController.java
+++ src/controller/VehiculeDetailController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 import javafx.util.Callback;
 import model.CommonFunctions;
@@ -38,7 +39,7 @@ public class VehiculeDetailController implements Initializable {
 	@FXML
 	private TableColumn<Entretien, BigDecimal> prixCol;
 	private ObservableList<Entretien> listeEntretiensVehicule;
-	
+
 	@FXML
 	private TextField noStockTextField;
 	@FXML
@@ -78,14 +79,18 @@ public class VehiculeDetailController implements Initializable {
 		System.out.println(VehiculeToBeModified.toText());
 		estVenduCombo.getItems().addAll("Oui", "Non");
 		setFieldsWithVehiculeData();
-		
+
 		try {
-			String sql = "SELECT * FROM Entretiens WHERE fldNoStock='"+VehiculeToBeModified.getStockNumber()+"'";
+			String sql = "SELECT * FROM Entretiens WHERE fldNoStock='"
+					+ VehiculeToBeModified.getStockNumber() + "'";
 			listeEntretiensVehicule = Entretien.listeDeEtretien(sql);
 			fillEntretienTable(listeEntretiensVehicule);
-			BigDecimal test = Entretien.getTotalEntretientVehicule(VehiculeToBeModified.getStockNumber());
-			totalEntretienLabel.setText("Total : "+CommonFunctions.displayCurrency(test)+" ");
-			
+			BigDecimal test = Entretien
+					.getTotalEntretientVehicule(VehiculeToBeModified
+							.getStockNumber());
+			totalEntretienLabel.setText("Total : "
+					+ CommonFunctions.displayCurrency(test) + " ");
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -122,67 +127,88 @@ public class VehiculeDetailController implements Initializable {
 		}
 		transmissionCombo.getEditor().setText(
 				VehiculeToBeModified.getTransmission());
-		prixAchatTextField.setText(CommonFunctions.displayCurrency(VehiculeToBeModified
-				.getPrixAcheteur()));
-		prixVenteTextField.setText(CommonFunctions.displayCurrency(VehiculeToBeModified.getPrixVente()));
+		prixAchatTextField.setText(CommonFunctions
+				.displayCurrency(VehiculeToBeModified.getPrixAcheteur()));
+		if (VehiculeToBeModified.isEstVendu() == true) {
+			prixVenteTextField.setText(CommonFunctions
+					.displayCurrency(VehiculeToBeModified.getPrixVente()));
+		}
+		double prixAvecEntretiens;
+		double prixEntretiens = Entretien.getTotalEntretientVehicule(
+				VehiculeToBeModified.getStockNumber()).doubleValue();
 
+		prixAvecEntretiens = VehiculeToBeModified.getPrixAcheteur()
+				.doubleValue() + prixEntretiens;
+		prixEntretiensTextField.setText(CommonFunctions
+				.displayCurrency(prixAvecEntretiens));
+		if (VehiculeToBeModified.isEstVendu() == true) {
+			double profitPerte = VehiculeToBeModified.getPrixVente()
+					.doubleValue() - prixAvecEntretiens;
+			profitPerteTextField.setText(CommonFunctions
+					.displayCurrency(profitPerte));
+		}
 	}
-	
-	public void fillEntretienTable(ObservableList<Entretien> listeEntretiens){
+
+	public void fillEntretienTable(ObservableList<Entretien> listeEntretiens) {
 		System.out.println("Nombre d'éléments :" + listeEntretiens.size());
 		try {
-			dateEntretienCol.setCellValueFactory(new PropertyValueFactory<Entretien, Date>("date"));
-			detaillantCol.setCellValueFactory(new PropertyValueFactory<Entretien, String>("nomSousContractant"));
-			descriptionCol.setCellValueFactory(new PropertyValueFactory<Entretien, String>("description"));
-			FormattedTableCellFactory<Entretien, String> pp  = new FormattedTableCellFactory<Entretien, String>();
+			dateEntretienCol
+					.setCellValueFactory(new PropertyValueFactory<Entretien, Date>(
+							"date"));
+			detaillantCol
+					.setCellValueFactory(new PropertyValueFactory<Entretien, String>(
+							"nomSousContractant"));
+			descriptionCol
+					.setCellValueFactory(new PropertyValueFactory<Entretien, String>(
+							"description"));
+			FormattedTableCellFactory<Entretien, String> pp = new FormattedTableCellFactory<Entretien, String>();
 			pp.setAlignment(TextAlignment.RIGHT);
 			noFactureCol.setCellFactory(pp);
-			noFactureCol.setCellValueFactory(new PropertyValueFactory<Entretien, String>("noFacture"));
-			prixCol.setCellValueFactory(new PropertyValueFactory<Entretien, BigDecimal>("cout"));
-			prixCol.setCellFactory(new Callback<TableColumn<Entretien,BigDecimal>, TableCell<Entretien,BigDecimal>>() {
-				
+			noFactureCol
+					.setCellValueFactory(new PropertyValueFactory<Entretien, String>(
+							"noFacture"));
+			prixCol.setCellValueFactory(new PropertyValueFactory<Entretien, BigDecimal>(
+					"cout"));
+			prixCol.setCellFactory(new Callback<TableColumn<Entretien, BigDecimal>, TableCell<Entretien, BigDecimal>>() {
+
 				@Override
-				 public TableCell call(TableColumn p) {
-	                TableCell cell = new TableCell<Entretien, BigDecimal>() {
-	                    @Override
-	                    public void updateItem(BigDecimal item, boolean empty) {
-	                        super.updateItem(item, empty);
-	                        setText(empty ? null : getString());
-	                        setGraphic(null);
-	                    }
+				public TableCell call(TableColumn p) {
+					TableCell cell = new TableCell<Entretien, BigDecimal>() {
+						@Override
+						public void updateItem(BigDecimal item, boolean empty) {
+							super.updateItem(item, empty);
+							setText(empty ? null : getString());
+							setGraphic(null);
+						}
 
-	                    private String getString() {
-	                        String ret = "";
-	                        if (getItem() != null) {
-	                            String gi = getItem().toString();
-	                            NumberFormat df = DecimalFormat.getInstance();
-	                            df.setMinimumFractionDigits(2);
-	                            df.setRoundingMode(RoundingMode.DOWN);
+						private String getString() {
+							String ret = "";
+							if (getItem() != null) {
+								String gi = getItem().toString();
+								NumberFormat df = DecimalFormat.getInstance();
+								df.setMinimumFractionDigits(2);
+								df.setRoundingMode(RoundingMode.DOWN);
 
-	                            ret = df.format(Double.parseDouble(gi));
-	                        } else {
-	                            ret = "0.00";
-	                        }
-	                        return ret;
-	                    }
-	                };
+								ret = df.format(Double.parseDouble(gi));
+							} else {
+								ret = "0.00";
+							}
+							return ret;
+						}
+					};
 
-	                cell.setStyle("-fx-alignment: center-right;");
-	                //cell.getStyleClass().add(("-fx-alignment: center-right;"));
-	               // cell.getStyleClass().add(("-fx-padding: 5px;"));
-	                return cell;
-	            }
+					cell.setStyle("-fx-alignment: center-right;");
+					// cell.getStyleClass().add(("-fx-alignment: center-right;"));
+					// cell.getStyleClass().add(("-fx-padding: 5px;"));
+					return cell;
+				}
 			});
-			
+
 			tableEntretien.setItems(listeEntretiens);
-			
-			
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getStackTrace());
 		}
 	}
-
-
 
 }
--- src/model/CommonFunctions.java
+++ src/model/CommonFunctions.java
@@ -7,8 +7,16 @@ import java.util.Locale;
 public final class CommonFunctions {
 
 	private CommonFunctions(){}
-	
+	// Surcharge de la fonction pour les type BigDecimal
 	public static String displayCurrency(BigDecimal value) {
+		String currencyValue = new String();
+		NumberFormat currencyFormat = NumberFormat
+				.getCurrencyInstance(Locale.CANADA);
+		currencyValue = currencyFormat.format(value);
+		return currencyValue;
+	}
+	// Surcharge de la fonction pour les type double
+	public static String displayCurrency(double value) {
 		String currencyValue = new String();
 		NumberFormat currencyFormat = NumberFormat
 				.getCurrencyInstance(Locale.CANADA);
