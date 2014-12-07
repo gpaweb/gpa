f5e9724248bcf073c2f1c791732de273d4874d77
eric <estpier@gmail.com>
2014-12-07 08:11:51 -0500

    7 décembre am avec netbeans

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,23 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			// BorderPane root = new BorderPane();
-			Parent root = FXMLLoader.load(getClass().getResource(
-					"/fxml/Login.fxml"));
-			Scene scene = new Scene(root);
-			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Gestion de parc automobile");
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // BorderPane root = new BorderPane();
+            Parent root = FXMLLoader.load(getClass().getResource(
+                    "/fxml/Login.fxml"));
+            Scene scene = new Scene(root);
+            // scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Gestion de parc automobile");
+            primaryStage.show();
+        } catch (Exception e) {
+            System.out.println("An error as append...");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
